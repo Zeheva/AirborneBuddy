e68bcbaa8560a87d9874556ee769095d5b1d5058
Zeheva <brendon.r.hollingsworth@outlook.com>
2017-06-22 10:58:18 -0400

    Add project files.

---
+++ AirborneBuddy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AirborneBuddy.MvcApplication" Language="C#" %>
---
+++ AirborneBuddy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AirborneBuddy.Startup))]
+namespace AirborneBuddy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
